c44a7b83abae6c38d9446efe3be5634b657f6261
Walter Arenas <walter@Walters-MacBook-Air.local>
2021-04-01 12:53:23 -0400

    Set up server.js and ccmanaager.js controller with GET route. Port 3000 listening. Local host rendering res.send

---
+++ controllers/ccmanager.js
@@ -0,0 +1,37 @@
+/////////////// DEPENDENCIES
+const express = require("express");
+const ROUTER = express.Router();
+
+ROUTER.get("/", (req, res)  => {
+    res.send("Ready to start the Credit Card Manager project!");
+})
+
+/////////////// MODELS
+
+
+/////////////// ROUTES
+
+// seed route
+
+
+// index route
+
+
+// new route
+
+// create route
+
+
+// show route
+
+
+// edit route
+
+
+// update route
+
+
+// delete route
+
+
+module.exports = ROUTER;
